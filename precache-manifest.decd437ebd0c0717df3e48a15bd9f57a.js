433a318a2e9fff661a3ab10eb0d9f5b8a81bcd8b
birolguldal <46013256+birolguldal@users.noreply.github.com>
2022-01-14 00:04:22 -0500

    Add files via upload

---
+++ precache-manifest.decd437ebd0c0717df3e48a15bd9f57a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0873f1af224841965229c558d4aa0e",
+    "url": "/art-portfolio/index.html"
+  },
+  {
+    "revision": "ba0fef93c048d7531fcc",
+    "url": "/art-portfolio/static/css/main.8c132934.chunk.css"
+  },
+  {
+    "revision": "17a49aab36a5335c2c79",
+    "url": "/art-portfolio/static/js/2.30932eee.chunk.js"
+  },
+  {
+    "revision": "ba0fef93c048d7531fcc",
+    "url": "/art-portfolio/static/js/main.cbe17609.chunk.js"
+  },
+  {
+    "revision": "8b3ab8278305737d033e",
+    "url": "/art-portfolio/static/js/runtime~main.4898935c.js"
+  }
+]);
